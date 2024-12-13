5f77a3bd79826849b3cef3ced5673f50ca90f783
Joshua Reyestan <jreyesta@stu.southtexascollege.edu>
2024-12-13 16:14:57 -0600

    fixed rounded corners on image and card

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -5,14 +5,14 @@ const firstname = faker.person.firstName()
 const lastname = faker.person.lastName()
 const job_title = faker.person.jobTitle()
 const bio = faker.lorem.lines(2)
-const username = faker.internet.userName({ firstName: firstname, lastName: lastname }).toLowerCase()
+const username = faker.internet.userName({ firstName:firstname, lastName: lastname }).toLowerCase()
 
 </script>
 
 <template>
     <!-- {{ fullname }} <br /> -->
     <div class="border border-gray-400 rounded-lg shadow bg-gray-200 h-full">
-        <img :src="faker.image.urlLoremFlickr({category: 'cat'})"/>
+        <img :src="faker.image.urlLoremFlickr({category: 'cat'})" class="rounded-t-xl"/>
         <div class="p-4">
             <p class="mb-2 text-2xl font-bold">{{ fullname }}</p>
             <p class="mb-3 font-normal text-gray-700">{{ job_title }}</p>
